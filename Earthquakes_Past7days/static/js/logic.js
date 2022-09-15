3dc1b719876a111fe12149e608747810c1b0adb3
Abi <aedwar216@gmail.com>
2022-09-15 16:53:45 -0600

    added legend and toggle

--- Earthquakes_Past7days/static/js/logic.js
+++ Earthquakes_Past7days/static/js/logic.js
@@ -20,15 +20,25 @@ let baseMaps = {
   "Satellite Streets": satelliteStreets
 };
 
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
   center: [39.5, -98.5],
   zoom: 3,
   layers: [satelliteStreets]
 })
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
 
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoHoods = "https://raw.githubusercontent.com/AbiE216/Mapping_Earthquakes/main/torontoNeighborhoods.json";
@@ -86,11 +96,43 @@ L.geoJSON(data, {
       return L.circleMarker(latlng);
     },
   // We set the style for each circleMarker using our styleInfo function.
-style: styleInfo,
+  style: styleInfo,
   // We create a popup for each circleMarker to display the magnitude and
   //  location of the earthquake after the marker has been created and styled.
   onEachFeature: function(feature, layer) {
-  layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
 }
-}).addTo(map);
+}).addTo(earthquakes);
+
+  //Then we add the earthquake layer
+  earthquakes.addTo(map);
+
+  // Create a legend control object.
+  let legend = L.control({
+    position: "bottomright"
+  });
+    // Then add all the details for the legend.
+  legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+  ];
+    // Looping through our intervals to generate a label with a colored square for each interval.
+    for (var i = 0; i < magnitudes.length; i++) {
+      console.log(colors[i]);
+      div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " +
+        magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+  }
+    return div;
+  };
+
+  legend.addTo(map);
+
 });
